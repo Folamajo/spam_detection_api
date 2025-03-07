0200fcac48a2889e358a5617db19a83bd7dd9146
fmajoyegbe@gmail.com <fmajoyegbe@gmail.com>
2025-03-07 17:05:38 +0000

    Project set up plan

---
+++ app/main.py
@@ -0,0 +1,5 @@
+#Step 1: Create FastAPI app
+# Step 2: Load Model and Vectoriser on Startup
+# Step 3: Define /predict Endpoint
+# Step 4: Handle Input Validation
+
---
+++ training/train_model.py
@@ -0,0 +1,15 @@
+# Step 1: Load Data from CSV
+# Step 2: Vectorize text
+# Step 3: Train Model
+# Step 4: Save Model and Vectorizer
+
+#Save models as models/spam_classifier.pkl
+# Save vectorizer as models/vectorizer.pkl
+#use joblib for both 
+
+#TESTING TRAINING PROCESS
+   #When we run train_model.py:
+      #Make sure no errors happen
+      # Check spam_classifier.pkl and vectorizer.pkl actually appear in your models/folder
+      # Check the file sizes they should not be empty
+      # print a sample prediction from the trained model.
