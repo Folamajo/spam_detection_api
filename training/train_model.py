593584e29ef685a4bc33919a383f5d6ddfee4a62
fmajoyegbe@gmail.com <fmajoyegbe@gmail.com>
2025-03-10 17:41:40 +0000

    Loaded dataset and Vectorised data

--- training/train_model.py
+++ training/train_model.py
@@ -13,3 +13,23 @@
       # Check spam_classifier.pkl and vectorizer.pkl actually appear in your models/folder
       # Check the file sizes they should not be empty
       # print a sample prediction from the trained model.
+
+
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+#Loading the dataset
+path_to_file ="/Users/fola/Documents/Projects/spam_detection_api/training/spam_data.csv"
+training_data = pd.read_csv(path_to_file)
+
+# print(training_data)
+
+vectorizer = TfidfVectorizer(lowercase=True, stop_words='english')
+transformed_data = vectorizer.fit_transform(training_data["Text"])
+print(transformed_data)
+#Prind the cshape this gives us rows and columns
+print(transformed_data.shape)
+print("Tranformed first text message", transformed_data.toarray()[0])
+
+# print(training_data.shape)
+# print(training_data.dtypes)
